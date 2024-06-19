25c3ff57494443f8d514b7fa25d1334b5cbf8b2a
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-19 21:08:45 +0500

    add:  tweet_edit created

--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -21,9 +21,23 @@ def tweet_create(request):
             tweet.user = request.user
             tweet.save()
             return redirect('tweet_list')
-        # aadded
         pass 
     else:
         form = TweetForm()
     
     return render(request, 'tweet_form.html',{'form':form})
+
+
+def tweet_edit(request,tweet_id):
+    tweet = get_object_or_404(Tweet,pk=tweet_id,user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST,request.FILES,instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html',{'form':form})
+    

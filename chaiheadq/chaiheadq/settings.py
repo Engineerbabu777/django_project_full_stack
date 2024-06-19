b762c23a53250ab6cff793de505131d29c34fdac
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-19 14:49:16 +0500

    Add: basic url,view,template

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'chaiheadq.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
